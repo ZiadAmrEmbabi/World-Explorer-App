a51f5a37f0a1d17b478d7d5b5d639e33e58e0172
ZiadAmrEmbabi <zamr@madfoat.com>
2024-06-06 12:41:09 +0300

    adding population

--- world-explore/src/app/home/home.page.ts
+++ world-explore/src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../models/model';
+import { CountryService } from '../services/country.service.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  countries: Country[] | any;
+  selectedCountryPopulation: { [country: string]: number | null } = {};
 
-  constructor() {}
 
+
+  constructor(private countryService: CountryService) { }
+
+  ngOnInit() {
+    this.loadCountries();
+  }
+
+  loadCountries() {
+    this.countryService.getCountries().subscribe(data => {
+      console.log(data);
+      this.countries = data.data;
+    });
+  }
+
+  showPopulation(country: Country) {
+    if (this.selectedCountryPopulation[country.country] !== undefined) {
+      // If population is already displayed, hide it
+      delete this.selectedCountryPopulation[country.country];
+    } else {
+      // Fetch and display the population
+      this.countryService.getPopulation(country.country).subscribe(data => {
+        const population = data.data.populationCounts.pop();
+        this.selectedCountryPopulation[country.country] = population.value;
+      });
+    }
+  }
 }
---
+++ world-explore/src/app/services/country.service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+  private apiUrl = 'https://countriesnow.space/api/v0.1/countries';
+
+  constructor(private http: HttpClient) { }
+
+  getCountries(): Observable<{ data: any[] }> {
+    return this.http.get<{ data: any[] }>(this.apiUrl);
+  }
+
+  getPopulation(country: string): Observable<any> {
+    const populationUrl = `${this.apiUrl}/population`;
+    return this.http.post(populationUrl, { country });
+  }
+}
